1fc956d692f7aefee8ef41018089c2d5363b4fd4
Timothée SANCHEZ <timothee.sanchez.faly@gmail.com>
2024-12-16 19:05:32 +0100

    UP my solution

--- pandas_questions.py
+++ pandas_questions.py
@@ -109,10 +109,8 @@ def plot_referendum_map(referendum_result_by_regions):
 if __name__ == "__main__":
     referendum, df_reg, df_dep = load_data()
     regions_and_departments = merge_regions_and_departments(df_reg, df_dep)
-    referendum_and_areas = merge_referendum_and_areas(
-        referendum, regions_and_departments
-    )
-    referendum_results = compute_referendum_result_by_regions(referendum_and_areas)
+    refer_and_areas = merge_referendum_and_areas(referendum, regions_and_departments)
+    referendum_results = compute_referendum_result_by_regions(refer_and_areas)
     print(referendum_results)
 
     plot_referendum_map(referendum_results)
