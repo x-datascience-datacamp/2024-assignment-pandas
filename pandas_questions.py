2fc711275ba3a89f29e1c9b8f5e735bcf62fc3d7
odd2022 <audrey.airaud@gmail.com>
2024-12-16 21:38:15 +0100

    UP my solution

--- pandas_questions.py
+++ pandas_questions.py
@@ -29,10 +29,12 @@ def merge_regions_and_departments(regions, departments):
     ['code_reg', 'name_reg', 'code_dep', 'name_dep']
     """
 
-    regions_and_departments = pd.merge(regions, departments, left_on="code", right_on="region_code")
-    regions_and_departments = regions_and_departments[["region_code", "name_x", "code_y", "name_y"]]
-    regions_and_departments.columns = ['code_reg', 'name_reg', 'code_dep', 'name_dep']
-    return pd.DataFrame(regions_and_departments)
+    reg_and_dep = pd.merge(
+        regions, departments, left_on="code", right_on="region_code"
+        )
+    reg_and_dep = reg_and_dep[["region_code", "name_x", "code_y", "name_y"]]
+    reg_and_dep.columns = ['code_reg', 'name_reg', 'code_dep', 'name_dep']
+    return pd.DataFrame(reg_and_dep)
 
 
 def merge_referendum_and_areas(referendum, regions_and_departments):
@@ -42,10 +44,16 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
     french living abroad.
     """
     reg_to_drop = ["COM", "01", "02", "03", "04", "05", "06"]
-    regions_and_departments = regions_and_departments[~regions_and_departments["code_reg"].isin(reg_to_drop)]
-    referendum = referendum[~referendum["Department code"].str.startswith("Z")]
-    referendum.loc[:, "Department code"]  = referendum["Department code"].replace({str(i): f"0{i}" for i in range(1, 10)})
-    referendum_and_areas = pd.merge(regions_and_departments, referendum, left_on="code_dep", right_on="Department code")
+    reg_and_dep = regions_and_departments[
+        ~regions_and_departments["code_reg"].isin(reg_to_drop)
+        ]
+    ref = referendum[~referendum["Department code"].str.startswith("Z")]
+    ref.loc[:, "Department code"] = ref["Department code"].replace(
+        {str(i): f"0{i}" for i in range(1, 10)}
+        )
+    referendum_and_areas = pd.merge(
+        reg_and_dep, ref, left_on="code_dep", right_on="Department code"
+        )
     return pd.DataFrame(referendum_and_areas)
 
 
@@ -55,10 +63,19 @@ def compute_referendum_result_by_regions(referendum_and_areas):
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
-    votes_by_department = referendum_and_areas.groupby("code_reg")[["Registered", "Abstentions", "Null", "Choice A", "Choice B"]].sum().reset_index()
-    unique_names = referendum_and_areas[["code_reg", "name_reg"]].drop_duplicates(subset="code_reg")
-    votes_by_department = pd.merge(votes_by_department, unique_names, on="code_reg", how="inner")
-    return pd.DataFrame(votes_by_department[['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']])
+    votes_by_department = referendum_and_areas.groupby("code_reg")[
+        ["Registered", "Abstentions", "Null", "Choice A", "Choice B"]
+        ].sum().reset_index()
+    unique_names = referendum_and_areas[
+        ["code_reg", "name_reg"]
+        ].drop_duplicates(subset="code_reg")
+    votes_by_department = pd.merge(
+        votes_by_department, unique_names, on="code_reg", how="inner"
+        )
+    return pd.DataFrame(votes_by_department[
+        ['name_reg', 'Registered', 'Abstentions',
+         'Null', 'Choice A', 'Choice B']
+        ])
 
 
 def plot_referendum_map(referendum_result_by_regions):
@@ -71,10 +88,18 @@ def plot_referendum_map(referendum_result_by_regions):
     * Return a gpd.GeoDataFrame with a column 'ratio' containing the results.
     """
     geographic_data = gpd.read_file("data/regions.geojson")
-    referendum_result_by_regions = pd.merge(geographic_data, referendum_result_by_regions, left_on="nom", right_on="name_reg", how="inner")
-    referendum_result_by_regions['ratio'] = referendum_result_by_regions['Choice A'] / (referendum_result_by_regions['Choice A'] + referendum_result_by_regions['Choice B'])
-    referendum_result_by_regions.plot(column='ratio', cmap='coolwarm', legend=True, figsize=(12, 8), edgecolor='black')
-    return gpd.GeoDataFrame(referendum_result_by_regions)
+    ref_by_reg = pd.merge(
+        geographic_data, referendum_result_by_regions,
+        left_on="nom", right_on="name_reg", how="inner"
+        )
+    ref_by_reg['ratio'] = ref_by_reg['Choice A'] / (
+        ref_by_reg['Choice A'] + ref_by_reg['Choice B']
+        )
+    ref_by_reg.plot(
+        column='ratio', cmap='coolwarm', legend=True,
+        figsize=(12, 8), edgecolor='black'
+        )
+    return gpd.GeoDataFrame(ref_by_reg)
 
 
 if __name__ == "__main__":
