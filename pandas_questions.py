b325423eae6972b7aaefb21bc7dec441a7282f45
Pasadobly <109421862+Pasadobly@users.noreply.github.com>
2024-12-18 00:41:44 +0100

    UP my solution

--- pandas_questions.py
+++ pandas_questions.py
@@ -15,9 +15,10 @@ import matplotlib.pyplot as plt
 
 def load_data():
     """Load data from the CSV files referundum/regions/departments."""
-    referendum = pd.read_csv('data/referendum.csv')
+    referendum = pd.read_csv('data/referendum.csv', sep=';')
     regions = pd.read_csv('data/regions.csv')
     departments = pd.read_csv('data/departments.csv')
+    departments['code'] = departments['code'].astype(str).str.lstrip('0')
 
     return referendum, regions, departments
 
@@ -30,7 +31,12 @@ def merge_regions_and_departments(regions, departments):
     """
     regions = regions.rename(columns={'code': 'code_reg', 'name': 'name_reg'})
     departments = departments.rename(columns={'region_code': 'code_reg', 'code': 'code_dep', 'name': 'name_dep'})
-    regions_and_departments = pd.merge(departments, regions, on='code_reg', how='inner')
+    regions_and_departments = pd.merge(
+        departments, 
+        regions, 
+        on='code_reg', 
+        how='inner'
+    )
     regions_and_departments = regions_and_departments[['code_reg', 'name_reg', 'code_dep', 'name_dep']]
     
     return regions_and_departments
@@ -42,13 +48,14 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
     You can drop the lines relative to DOM-TOM-COM departments, and the
     french living abroad.
     """
-    referendum = referendum[referendum['Department code'].str.isdigit()]
+    #referendum = referendum[referendum['Department code'].str.isdigit()]
     referendum_and_areas = referendum.merge(
         regions_and_departments,
         left_on='Department code', 
         right_on='code_dep', 
         how='inner'
     )
+    referendum_and_areas = referendum_and_areas.dropna()
 
     return referendum_and_areas
 
@@ -59,14 +66,9 @@ def compute_referendum_result_by_regions(referendum_and_areas):
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
-    referendum_result_by_regions = referendum_and_areas.groupby('code_reg').agg({
-        'name_reg': 'first',
-        'Registered': 'sum',
-        'Abstentions': 'sum',
-        'Null': 'sum',
-        'Choice A': 'sum',
-        'Choice B': 'sum'
-    }).reset_index(drop=True)
+    cols = ['Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
+    referendum_result_by_regions = referendum_and_areas.groupby(['code_reg', 'name_reg'])[cols].sum().reset_index()
+    referendum_result_by_regions = referendum_result_by_regions.set_index('code_reg')
 
     return referendum_result_by_regions
 
@@ -82,12 +84,17 @@ def plot_referendum_map(referendum_result_by_regions):
     """
     regions_geo = gpd.read_file('data/regions.geojson')
     
-    merged = regions_geo.merge(referendum_result_by_regions, left_on='code', right_on='code_reg', how='left')
+    merged = regions_geo.merge(
+        referendum_result_by_regions, 
+        left_on='code', 
+        right_on='code_reg', 
+        how='left'
+    )
 
-    merged['ratio'] = merged['Choice A'] / (merged['Choice A'] + merged['Choice B'] + merged['Null'])
+    merged['ratio'] = merged['Choice A'] / (merged['Choice A'] + merged['Choice B'])
 
-    merged.plot(column='ratio', cmap='coolwarm', legend=True,
-                     legend_kwds={'label': "Rate of 'Choice A'", 'orientation': "horizontal"})
+    merged = gpd.GeoDataFrame(merged)
+    merged.plot(column="ratio", legend=True, cmap="RdYlBu")
     
     return merged
 
