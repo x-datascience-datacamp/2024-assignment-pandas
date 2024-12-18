738ef411a4292e269f6a451fcd31242d7a05f757
rouaabl <rouaa.blel@ensae.fr>
2024-12-19 00:49:24 +0100

    UP my solution

--- pandas_questions.py
+++ pandas_questions.py
@@ -9,27 +9,32 @@ To do that, you will load the data as pandas.DataFrame, merge the info and
 aggregate them by regions and finally plot them on a map using `geopandas`.
 """
 import pandas as pd
-import geopandas as gpd
+import geopandas as gppytesd
 import matplotlib.pyplot as plt
 
 
 def load_data():
     """Load data from the CSV files referundum/regions/departments."""
-    referendum = pd.DataFrame({})
-    regions = pd.DataFrame({})
-    departments = pd.DataFrame({})
+    referendum = pd.read_csv('data/referendum.csv', sep=';')
+    regions = pd.read_csv('data/regions.csv', sep=',')
+    departments = pd.read_csv('data/departments.csv', sep=',')
 
     return referendum, regions, departments
 
 
 def merge_regions_and_departments(regions, departments):
-    """Merge regions and departments in one DataFrame.
+    """
+    Merge regions and departments in one DataFrame.
 
     The columns in the final DataFrame should be:
     ['code_reg', 'name_reg', 'code_dep', 'name_dep']
     """
+    regions_and_departments = pd.merge(departments, regions, left_on='region_code', right_on='code', how='inner')
+    regions_and_departments = regions_and_departments.rename(columns={'code_y': 'code_reg', "name_y": 'name_reg','code_x': 'code_dep',"name_x": 'name_dep'})
+    regions_and_departments = regions_and_departments[['code_reg', 'name_reg', 'code_dep', 'name_dep']]
 
-    return pd.DataFrame({})
+    return regions_and_departments
+
 
 
 def merge_referendum_and_areas(referendum, regions_and_departments):
@@ -38,8 +43,25 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
     You can drop the lines relative to DOM-TOM-COM departments, and the
     french living abroad.
     """
+    
 
-    return pd.DataFrame({})
+    #referendum_filtered = referendum[(referendum['Department code'].isin(['ZA','ZB','ZC','ZD','ZM','ZN','ZP','ZS','ZW','ZX',"ZZ"]))==False]
+    regions_and_departments = regions_and_departments[(regions_and_departments['code_reg'].isin(['DOM','COM','TOM']))==False]
+    referendum_filtered = referendum[referendum['Department code'] != 'FRANCAIS DE L\'ETRANGER']
+    regions_and_departments ['code_dep'] =regions_and_departments ['code_dep'].apply(lambda x: x.lstrip('0') if x.isnumeric() else x)
+
+
+
+    referendum_and_areas = pd.merge(
+        referendum_filtered,
+        regions_and_departments,
+        left_on='Department code',
+        right_on='code_dep',
+        how='inner'
+    )
+
+
+    return referendum_and_areas
 
 
 def compute_referendum_result_by_regions(referendum_and_areas):
@@ -48,9 +70,17 @@ def compute_referendum_result_by_regions(referendum_and_areas):
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
+    grouped =  referendum_and_areas.groupby('code_reg', as_index=True).agg({
+        'name_reg': 'first',      
+        'Registered': 'sum',      
+        'Abstentions': 'sum',
+        'Null': 'sum',
+        'Choice A': 'sum',
+        'Choice B': 'sum'
+    })
+    result = grouped[['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']]
 
-    return pd.DataFrame({})
-
+    return result
 
 def plot_referendum_map(referendum_result_by_regions):
     """Plot a map with the results from the referendum.
@@ -61,8 +91,16 @@ def plot_referendum_map(referendum_result_by_regions):
       should display the rate of 'Choice A' over all expressed ballots.
     * Return a gpd.GeoDataFrame with a column 'ratio' containing the results.
     """
+    geo_data = gppytesd.read_file("data/regions.geojson")
 
-    return gpd.GeoDataFrame({})
+    merged_data = geo_data.merge(referendum_result_by_regions, how="left", left_on="code", right_index=True)
+
+    merged_data["ratio"] = merged_data["Choice A"] / (merged_data["Choice A"] + merged_data["Choice B"])
+
+    merged_data.plot(column="ratio", legend=True, cmap="coolwarm", figsize=(10, 10))
+    
+    return merged_data
+
 
 
 if __name__ == "__main__":
