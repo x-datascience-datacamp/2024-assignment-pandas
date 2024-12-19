edf924ca52effe7fe14adeee6a785376cb98a111
MahammadNuriyev62 <maganuriyev@gmail.com>
2024-12-19 01:10:36 +0100

    refactor: remove unnecessary blank lines in pandas_questions.py

--- pandas_questions.py
+++ pandas_questions.py
@@ -33,7 +33,6 @@ def merge_regions_and_departments(regions, departments):
     The columns in the final DataFrame should be:
     ['code_reg', 'name_reg', 'code_dep', 'name_dep']
     """
-
     return pd.merge(
         departments,
         regions,
@@ -50,7 +49,6 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
     You can drop the lines relative to DOM-TOM-COM departments, and the
     french living abroad.
     """
-
     df = pd.merge(
         referendum,
         regions_and_departments,
@@ -69,7 +67,6 @@ def compute_referendum_result_by_regions(referendum_and_areas):
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
-
     result = referendum_and_areas.groupby("code_reg").agg(
         {
             "name_reg": "first",
@@ -92,7 +89,6 @@ def plot_referendum_map(referendum_result_by_regions):
       should display the rate of 'Choice A' over all expressed ballots.
     * Return a gpd.GeoDataFrame with a column 'ratio' containing the results.
     """
-
     regions_geo = gpd.read_file("./data/regions.geojson")
     regions_geo = regions_geo.merge(
         referendum_result_by_regions, left_on="code", right_on="code_reg"
