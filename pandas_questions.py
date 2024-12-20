7e6d29b83df4a9669d9142771c304013300da7c0
Kshitij-Ambilduke <kshitijambilduke1@gmail.com>
2024-12-20 18:27:04 +0000

    formatting

--- pandas_questions.py
+++ pandas_questions.py
@@ -32,9 +32,8 @@ def merge_regions_and_departments(regions, departments):
     region_temp = regions.drop(columns=["id", "slug"]).rename(
         columns={"code": "code_reg", "name": "name_reg"}
     )
-    dept_temp = departments.drop(columns=["id", "slug"]).rename(
-        columns={"code": "code_dep", "name": "name_dep", "region_code": "code_reg"}
-    )
+    t = {"code": "code_dep", "name": "name_dep", "region_code": "code_reg"}
+    dept_temp = departments.drop(columns=["id", "slug"]).rename(columns=t)
     df = pd.merge(region_temp, dept_temp, on="code_reg", how="left")
 
     return df
@@ -46,48 +45,37 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
     You can drop the lines relative to DOM-TOM-COM departments, and the
     french living abroad.
     """
-
-    regions_and_departments = regions_and_departments[
-        ~(regions_and_departments["code_reg"] == "COM")
-    ]
-    referendum.loc[referendum["Department code"].str.len() == 1, "Department code"] = (
-        referendum["Department code"].str.zfill(2)
-    )
-    referendum = referendum[~referendum["Department code"].str.match("^[Z]")]
+    t = regions_and_departments["code_reg"] == "COM"
+    regions_and_departments = regions_and_departments[~t]
+    c = "Department code"
+    t = referendum[c].str.len() == 1
+    referendum.loc[t, c] = referendum[c].str.zfill(2)
+    referendum = referendum[~referendum[c].str.match("^[Z]")]
     # quit()
 
     df = pd.merge(
         regions_and_departments,
         referendum,
         left_on="code_dep",
-        right_on="Department code",
+        right_on=c,
         how="right",
     )
 
     return df
 
 
-def compute_referendum_result_by_regions(referendum_and_areas):
+def compute_referendum_result_by_regions(smol):
     """Return a table with the absolute count for each region.
 
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
-    referendum_and_areas = referendum_and_areas.set_index("code_reg")
-    referendum_and_areas[
-        ["Registered", "Abstentions", "Choice A", "Choice B", "Null"]
-    ] = referendum_and_areas[
-        ["Registered", "Abstentions", "Choice A", "Choice B", "Null"]
-    ].astype(
-        int
-    )
-    referendum_result = (
-        referendum_and_areas[
-            ["name_reg", "Registered", "Abstentions", "Choice A", "Choice B", "Null"]
-        ]
-        .groupby(["code_reg", "name_reg"])
-        .sum()
-    )
+    smol = smol.set_index("code_reg")
+    t = ["Registered", "Abstentions", "Choice A", "Choice B", "Null"]
+    smol[t] = smol[t].astype(int)
+    t = ["name_reg", "Registered", "Abstentions",
+         "Choice A", "Choice B", "Null"]
+    referendum_result = smol[t].groupby(["code_reg", "name_reg"]).sum()
     referendum_result = referendum_result.reset_index().set_index("code_reg")
     return referendum_result
 
@@ -103,7 +91,8 @@ def plot_referendum_map(referendum_result_by_regions):
     """
     geo_df = gpd.read_file("data/regions.geojson")
     merged_df = geo_df.merge(
-        referendum_result_by_regions, how="inner", left_on="code", right_on="code_reg"
+        referendum_result_by_regions,
+        how="inner", left_on="code", right_on="code_reg"
     )
     merged_df["ratio"] = merged_df["Choice A"] / (
         merged_df["Choice A"] + merged_df["Choice B"]
@@ -122,7 +111,8 @@ if __name__ == "__main__":
     referendum_and_areas = merge_referendum_and_areas(
         referendum, regions_and_departments
     )
-    referendum_results = compute_referendum_result_by_regions(referendum_and_areas)
+    referendum_results = compute_referendum_result_by_regions(
+        referendum_and_areas)
     print(referendum_results)
 
     plot_referendum_map(referendum_results)
