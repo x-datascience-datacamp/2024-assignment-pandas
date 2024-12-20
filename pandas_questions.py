eba2ee4904705363fd50f3ffeee7d484d4b77982
Eya JLASSI_Codes <eya.jlassi@ensta-paris.fr>
2024-12-20 16:51:45 +0100

    pandas_questions.py

--- pandas_questions.py
+++ pandas_questions.py
@@ -81,7 +81,7 @@ def plot_referendum_map(referendum_result_by_regions):
       should display the rate of 'Choice A' over all expressed ballots.
     * Return a gpd.GeoDataFrame with a column 'ratio' containing the results.
     """
-    geo_regions = gpd.read_file('Data/regions.geojson')
+    geo_regions = gpd.read_file('data/regions.geojson')
 
     merged = geo_regions.merge(
         referendum_result_by_regions,
